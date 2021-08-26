cc372167b7589264fb096f449ed187267fb32948
Xukun Xiang <xxk08300190024@gmail.com>
2021-08-26 01:51:13 -0500

    initial successful deploy

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": process.env.PG_USERNAME,
+    "password": process.env.PG_PASSWORD,
+    "database": process.env.PG_DATABASE,
+    "host": process.env.PG_HOST,
     "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "prod": {
     "username": "",
